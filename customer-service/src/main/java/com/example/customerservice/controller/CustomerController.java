478cf12918dcd17e27e90805631117953d975f83
praveen-rps <109707042+praveen-rps@users.noreply.github.com>
2024-07-02 15:05:36 +0530

    Add files via upload

--- customer-service/src/main/java/com/example/customerservice/controller/CustomerController.java
+++ customer-service/src/main/java/com/example/customerservice/controller/CustomerController.java
@@ -1,7 +1,10 @@
 package com.example.customerservice.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +18,13 @@ import com.example.customerservice.model.Customer;
 import com.example.customerservice.model.OrdersDto;
 import com.example.customerservice.service.CustomerService;
 
+import io.github.resilience4j.retry.annotation.Retry;
+
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	Logger logger = LoggerFactory.getLogger(CustomerController.class);
 	
 	@Autowired
 	CustomerService service;
@@ -43,8 +50,20 @@ public class CustomerController {
 	}
 	
 	@GetMapping("/orders/{customerId}")
+	@Retry(name="customers", fallbackMethod="viewCustomerOrdersFallback")
 	public List<OrdersDto> viewCustomerOrders(@PathVariable Long customerId) {
+		logger.info("CustomerController is called");
 		return service.viewCustomerOrders(customerId);
+	}
+	
+	public List<OrdersDto> viewCustomerOrdersFallback(Long customerId, Exception e) {
+		List<OrdersDto> data = new ArrayList();
+		data.add(new OrdersDto(11111L,"Dummy Product1",22222L));
+		data.add(new OrdersDto(11112L,"Dummy Product2",32222L));
+		data.add(new OrdersDto(11113L,"Dummy Product3",42222L));
+		data.add(new OrdersDto(11114L,"Dummy Product4",52222L));
+		
+		return data;
 	}
 
 }
