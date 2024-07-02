3e8838919e8ef5cd2128c66895370eb7e8ddb7af
praveen-rps <109707042+praveen-rps@users.noreply.github.com>
2024-07-02 11:53:02 +0530

    Add files via upload

---
+++ citigateway/src/main/java/com/example/gateway/GateConfigs.java
@@ -0,0 +1,24 @@
+package com.example.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GateConfigs {
+	
+	@Bean
+	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+		
+		return builder.routes()
+		.route(p -> p.path("/customer/**")
+                .uri("lb://CUSTOMER-SERVICE/customer"))
+		.route(p -> p.path("/orders/**")
+				.uri("lb://ORDER-SERVICE/orders"))
+		.build();
+				
+	}
+
+}
+// localhost:8765/commets/all
